0d9d7be29c44f898d07bbd1472cd0546c312b78b
rebeccaBullTxt <dukeyuanl@gmail.com>
2024-05-30 09:10:20 +0800

    Update index.cjs
    小鲸崽子

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小鲸崽子',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o7PUA63uyhpELHbHo9GUo7WVHWxE',
+      id: 'o7PUA67VazQCJTGmFdywFyqR1rek',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '8dX87QXSGkdCmIpcGi16omBzWfi0DSNLDz5Ufvrd_2M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
